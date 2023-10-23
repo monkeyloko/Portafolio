8f31f307b1e3f75d80d02a479c94bc98fd8a4908
SANTIAGO DOFF <46873259@ort>
2023-10-23 14:30:19 -0300

    create react app

---
+++ portfolio/src/components/Layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./Layout.css"; // Asegúrate de crear un archivo Layout.css para los estilos.
+
+const Layout = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <li className="navbar-item">
+                        <Link to="/" className="navbar-link">
+                            Home
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </>
+    );
+};
+
+export default Layout;
