ab89eccaf42b26e1c245c059c91fcc3ef1151ddb
SANTIAGO DOFF <46873259@ort>
2023-11-15 12:02:28 -0300

    avance

--- portfolio/src/components/Creaciones.js
+++ portfolio/src/components/Creaciones.js
@@ -1,5 +1,6 @@
 import React from 'react';  
 import "./Creaciones.css"
+import CreacionesJSON from './CreacionesJSON';
 
 const Creaciones = () => {
     return (
---
+++ portfolio/src/components/CreacionesJSON.js
@@ -0,0 +1,35 @@
+const CreacionesJSON = [
+    {
+    "titulo": "creacion 1",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  },{
+    "titulo": "creacion 2",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  },{
+    "titulo": "creacion 3",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  },{
+    "titulo": "creacion 4",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  },{
+    "titulo": "creacion 5",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  },{
+    "titulo": "creacion 6",
+    "foto": "foto.png",
+    "descripcion": "texto aca",
+    "link": "github.com/monkeyloko"
+  }
+]
+
+export default CreacionesJSON;
