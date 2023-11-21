d5163d886a1f4549c24503b8363d066c4f4b727f
SANTIAGO DOFF <46873259@ort>
2023-11-21 11:34:01 -0300

    avance

--- portfolio/src/components/CreacionesJSON.js
+++ portfolio/src/components/CreacionesJSON.js
@@ -1,35 +1,40 @@
 const CreacionesJSON = [
-    {
+  {
     "titulo": "Pizzas API",
-    "foto": "../pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
-  },{
-    "titulo": "creacion 2",
-    "foto": "pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
-  },{
-    "titulo": "creacion 3",
-    "foto": "pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
-  },{
-    "titulo": "creacion 4",
-    "foto":"pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
-  },{
-    "titulo": "creacion 5",
-    "foto": "pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
-  },{
-    "titulo": "creacion 6",
-    "foto": "./src/pizza.jpg",
-    "descripcion": "texto aca",
-    "link": "github.com/monkeyloko"
+    "foto": "/pizza.jpg",
+    "descripcion": "API para gestionar información relacionada con pizzas. Permite operaciones CRUD con eficiencia.",
+    "link": "github.com/monkeyloko/pizzas-api"
+  },
+  {
+    "titulo": "Gestión de Autos",
+    "foto": "/autos.jpg",
+    "descripcion": "Sistema completo para gestionar autos. Desde registro hasta informes. Backend y frontend.",
+    "link": "github.com/monkeyloko/gestion-autos"
+  },
+  {
+    "titulo": "Hardware",
+    "foto": "/hardware.jpeg",
+    "descripcion": "Proyecto de desarrollo y programación de hardware. Explora sistemas embebidos.",
+    "link": "github.com/monkeyloko/hardware-project"
+  },
+  {
+    "titulo": "Banderas",
+    "foto": "/pizza.jpg",
+    "descripcion": "App interactiva para explorar y aprender sobre banderas de países.",
+    "link": "github.com/monkeyloko/banderas-app"
+  },
+  {
+    "titulo": "Catálogo React",
+    "foto": "/pizza.jpg",
+    "descripcion": "Catálogo de productos en React con Context API. Visualiza, busca y agrega productos al carrito.",
+    "link": "github.com/monkeyloko/catalogo-productos"
+  },
+  {
+    "titulo": "TP Personajes",
+    "foto": "/pizza.jpg",
+    "descripcion": "API para manejar información de personajes. Estructura robusta para datos de personajes.",
+    "link": "github.com/monkeyloko/tp-personajes-api"
   }
-]
+];
 
 export default CreacionesJSON;
--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import "./Home.css"; 
-import avatar from "./avatar.png"
 import { useCreacionesContext } from '../contextState';
 
 const Home = () => {
   const { creaciones } = useCreacionesContext();
   const primerasTresCreaciones = creaciones.slice(0, 3);
 
-
   return (
     <>
       <div className="home">
-        <div className="card">
+        <div className="cardTitulo">
           <h1 className="heading">
-            {console.log(creaciones)}
             Santiago Doff
           </h1>
           <p className="subheading">Desarrollador Front-end</p>
@@ -24,7 +20,7 @@ const Home = () => {
           <div key={creacion.titulo} className="card">
             <img src={creacion.foto} alt={creacion.titulo} />
             <h2>{creacion.titulo}</h2>
-            <p>{creacion.descripcion}</p>
+            <p></p>
             <a href={creacion.link} target="_blank" rel="noopener noreferrer">
               Ver en GitHub
             </a>
